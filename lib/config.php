f4aaf09e25b6a2f0c42328212a7f921bb9544c3c
M. Braun <michael-dev@fami-braun.de>
2012-11-01 16:06:29 +0100

    wahlen2012

--- lib/config.php
+++ lib/config.php
@@ -4,7 +4,7 @@ global $db_uri, $realm, $db_host, $tempdir;
 
 $db_host="localhost";
 
-$db_uri = 'mysql:host='.$db_host.';dbname=wahlen2011';
+$db_uri = 'mysql:host='.$db_host.';dbname=wahlen2012';
 $realm = 'Wahlen 2011';
 #$script_dir = dirname(dirname(__FILE__));
 #$tempdir = $script_dir.'/tmp/';
