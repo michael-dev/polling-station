523a88608c6b7e32d44a8bbd9462c0c4e24a936a
M. Braun <michael-dev@fami-braun.de>
2012-11-01 15:59:59 +0100

    move config to cfg, add changes for 2012

--- action/wahlbeteiligungabgabe.php
+++ action/wahlbeteiligungabgabe.php
@@ -3,7 +3,7 @@
 require "../lib/auth.php";
 
 $sth1 = newStm("SELECT f.id as id, name as name, (SELECT COUNT(*) FROM stimmberechtigt b WHERE b.fakultaet = f.id AND freigegeben = 1) AS gesamt, (SELECT COUNT(*) FROM stimmberechtigt b INNER JOIN eingesammelt s ON s.id = b.id WHERE b.fakultaet = f.id) AS abgegeben FROM fakultaet f");
-$sth2 = newStm("SELECT a.zettel as zettel, COUNT(*) AS ausgegeben FROM stimmberechtigt b INNER JOIN ausgabezettel a ON a.id = b.id GROUP BY a.zettel ORDER BY a.zettel");
+$sth2 = newStm("SELECT a.zettel as zettel, COUNT(*) AS ausgegeben FROM stimmberechtigt b INNER JOIN ausgabezettel a ON a.id = b.id INNER JOIN eingesammelt s ON s.id = b.id GROUP BY a.zettel ORDER BY a.zettel");
 execStm($sth1,Array());
 execStm($sth2,Array());
 
---
+++ js/zettelconfigparser.js
@@ -0,0 +1,52 @@
+var zettelconfig = null
+
+function zettelconfiginit() {
+  $.ajax('lib/config.ini', {'async': false})
+   .success(function (values, status, req) {
+     cfglines = values.split(/\r?\n/);
+     zettelconfig = {};
+     var section = '';
+     for (var k in cfglines) {
+       var line = cfglines[k];
+       line = line.replace(/#.*/, "");
+       line = line.replace(/[\r\n]/g, "");
+       line = line.replace(/^\s*/g, "");
+       if (line == '') { continue; }
+       // parse section header
+       var m = line.match(/^\[(.*)\]$/);
+       if (m) {
+         section = m[1];
+         zettelconfig[section] = {};
+         continue;
+       }
+       // assignments
+       if (!zettelconfig[section]) { zettelconfig[section] = {}; }
+       var m = line.match(/^([^=]*)=(.*)$/);
+       if (!m) {
+         m = [line,line,true];
+       }
+       if (m[1].substr(-2) == '[]') {
+         m[1] = m[1].substring(0,m[1].length-2);
+         if (!zettelconfig[section][m[1]]) { zettelconfig[section][m[1]] = []; }
+         zettelconfig[section][m[1]].push(m[2]);
+       } else {
+         zettelconfig[section][m[1]] = m[2];
+       }
+     }
+    })
+   .error(function () { alert("Die Stimmzettel konnten nicht geladen werden."); });
+}
+
+if (!Object.keys) {
+    Object.keys = function (obj) {
+        var keys = [],
+            k;
+        for (k in obj) {
+            if (Object.prototype.hasOwnProperty.call(obj, k)) {
+                keys.push(k);
+            }
+        }
+        return keys;
+    };
+}
+
