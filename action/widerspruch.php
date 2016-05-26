29849fb922dcb6856baa7d1214dcdbc433a6b787
Michael Braun <michael-dev@fami-braun.de>
2016-05-26 12:37:58 +0200

    fix sql error

--- action/widerspruch.php
+++ action/widerspruch.php
@@ -8,13 +8,14 @@ foreach ($_REQUEST as $k => $v)
  $txt .= " $k => $v";
 dblog("widerspruch $txt");
 
-$sth = newStm("INSERT INTO stimmberechtigt (mtknr, namenszusatz, vorname, nachname, geburtsdatum, fakultaet, studiengang) VALUES (:mtknr, :namenszusatz, :vorname, :nachname, :geburtsdatum, :fakultaet, :studiengang)");
+$sth = newStm("INSERT INTO stimmberechtigt (mtknr, namenszusatz, vorname, nachname, geburtsdatum, fakultaet, studiengang, freigegeben) VALUES (:mtknr, :namenszusatz, :vorname, :nachname, :geburtsdatum, :fakultaet, :studiengang, :freigegeben)");
 execStm($sth,Array(":mtknr" => $_REQUEST["wMatrikel"],
                     ":vorname" => $_REQUEST["wVorname"],
                     ":namenszusatz" => $_REQUEST["wNamenszusatz"],
                     ":nachname" => $_REQUEST["wNachname"],
                     ":geburtsdatum" => $_REQUEST["wGeburtsdatum"],
                     ":fakultaet" => $_REQUEST["wFak"],
-                    ":studiengang" => $_REQUEST["wStudiengang"]));
+                    ":studiengang" => $_REQUEST["wStudiengang"],
+                    ":freigegeben" => 0));
 execStm(newStm("COMMIT;"),Array());
 ?>
