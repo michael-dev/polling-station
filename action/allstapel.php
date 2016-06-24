194123a49844367c5a177f851c29f6ab3e9cbe56
Michael Braun <michael-dev@fami-braun.de>
2016-06-24 18:09:08 +0200

    more admin stats

--- action/allstapel.php
+++ action/allstapel.php
@@ -8,7 +8,7 @@ $filter = $_REQUEST["filter"];
 if ($wahlId == "null" || $wahlId === null || $wahlId === "" || $wahlId == -1) $wahlId = false;
 if ($filter == "null" || $filter === null || $filter === "" || $filter == -1) $filter = false;
 
-$sql = "Select w.name as wahlname, w.id as wahl, s.id as id, s.numVotes as numVotes, (SELECT COUNT(*) FROM stimmzettel z WHERE z.stapel = s.id) as numBallot, (SELECT MIN(numOk) FROM stimmzettel z WHERE z.stapel = s.id ) as minVerify from stapel s inner join wahl w on s.wahl = w.id having 1 ". ($wahlId !== false ? " AND wahl = ". ((int)$wahlId) : "") . ($filter !== false ? " AND (not exists(select * from stimmzettel zz where zz.stapel = s.id) OR minVerify <= " . ((int)$filter). ")" : ""). " order by wahl DESC, id ASC";
+$sql =    "Select w.name as wahlname, w.id as wahl, s.id as id, s.numVotes as numVotes, (SELECT COUNT(*) FROM stimmzettel z WHERE z.stapel = s.id) as numBallot, (SELECT MIN(numOk) FROM stimmzettel z WHERE z.stapel = s.id ) as minVerify, (SELECT COUNT(*) FROM stimmzettel z WHERE z.stapel = s.id ".($filter !== false ? "AND numOk <= ". ((int)$filter) : "").") as todoBallot, (SELECT COUNT(*) FROM stimmzettel z WHERE z.stapel = s.id ".($filter !== false ? "AND numOk > ". ((int)$filter) : "").") as doneBallot from stapel s inner join wahl w on s.wahl = w.id having 1 ". ($wahlId !== false ? " AND wahl = ". ((int)$wahlId) : "") . ($filter !== false ? " AND (not exists(select * from stimmzettel zz where zz.stapel = s.id) OR minVerify <= " . ((int)$filter). ")" : ""). " order by wahl DESC, id ASC";
 $sth = newStm($sql);
 
 $r = execStm($sth,Array());
